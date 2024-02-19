4638b54a9f4b626523230032c82520a4484eb1b4
ly thi my linh <linhdtnt2002@gmail.com>
2024-02-19 12:09:50 +0900

    sua lai

--- animal.py
+++ animal.py
@@ -1,2 +1,10 @@
-class Animal:
-    pass
+class Animals:
+    group =''
+    food= ''
+    def __init__(self,group,food):
+        self.group = group
+        self.food = food
+cow = Animals("mammal","grass")
+crocodile = Animals("reptiles", "meats")
+print("Cows belong to the group of  {} and eat {}".format(cow.group,cow.food))
+print("crocodile belong to the group of  {} and eat {}".format(crocodile.group,crocodile.food))
