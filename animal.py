f6c3336844bdbac8f4c4083f4ea926ef3c61d9fe
ly thi my linh <linhdtnt2002@gmail.com>
2024-02-19 12:36:41 +0900

    sua bai tap

--- animal.py
+++ animal.py
@@ -8,3 +8,21 @@ cow = Animals("mammal","grass")
 crocodile = Animals("reptiles", "meats")
 print("Cows belong to the group of  {} and eat {}".format(cow.group,cow.food))
 print("crocodile belong to the group of  {} and eat {}".format(crocodile.group,crocodile.food))
+class Bird(Animals):
+    def __init__(self):
+        print("Bird is ready")
+    def whatisthis(self):
+        print("Bird")
+    def fly(self):
+        print("fly so fast")
+class Eagle(Bird):
+    def __init__(self):
+        print("Eagle is ready")
+    def whatisthis(self):
+        print("Eagle")
+    def run(self):
+        print("run so fast")
+eagle=Eagle()
+eagle.whatisthis()
+eagle.fly()
+eagle.run()
