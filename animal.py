2fe3e0b0aa90d47c6e330ae689e8f0dda8961e35
ly thi my linh <linhdtnt2002@gmail.com>
2024-02-19 12:48:19 +0900

    them chu thich

--- animal.py
+++ animal.py
@@ -8,14 +8,14 @@ cow = Animals("mammal","grass")
 crocodile = Animals("reptiles", "meats")
 print("Cows belong to the group of  {} and eat {}".format(cow.group,cow.food))
 print("crocodile belong to the group of  {} and eat {}".format(crocodile.group,crocodile.food))
-class Bird(Animals):
+class Bird(Animals): #class Bird là class con của class Animals 
     def __init__(self):
         print("Bird is ready")
     def whatisthis(self):
         print("Bird")
     def fly(self):
         print("fly so fast")
-class Eagle(Bird):
+class Eagle(Bird):  # class Eagle là class con của Class Bird
     def __init__(self):
         print("Eagle is ready")
     def whatisthis(self):
@@ -24,5 +24,6 @@ class Eagle(Bird):
         print("run so fast")
 eagle=Eagle()
 eagle.whatisthis()
-eagle.fly()
+eagle.fly() #phương thức fly nằm trong class Bird nhưng class con Eagle vẫn có thể gọi sử dụng được
 eagle.run()
+
